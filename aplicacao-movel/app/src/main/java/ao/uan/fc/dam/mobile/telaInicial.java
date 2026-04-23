b84fe41893a776bbbe779c2bb85c0b5e711e9fcb
mutooca <mutoocamaria@gmail.com>
2026-04-23 14:13:54 +0100

    Atualização da tela inicial

--- aplicacao-movel/app/src/main/java/ao/uan/fc/dam/mobile/telaInicial.java
+++ aplicacao-movel/app/src/main/java/ao/uan/fc/dam/mobile/telaInicial.java
@@ -1,6 +1,7 @@
 package ao.uan.fc.dam.mobile;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class telaInicial extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_inicial);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnRegistar = findViewById(R.id.btnRegistar);
+        Button btnEntrar = findViewById(R.id.btnEntrar);
+
+        // 2. Ação do botão Registar
+        btnRegistar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(telaInicial.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnEntrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(telaInicial.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
