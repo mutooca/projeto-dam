498b2a059b20f6f4bc428f0c829933f9cf1f402f
mutooca <mutoocamaria@gmail.com>
2026-04-23 15:31:46 +0100

    Atualização do código

--- aplicacao-movel/app/src/main/java/ao/uan/fc/dam/mobile/telaInicial.java
+++ aplicacao-movel/app/src/main/java/ao/uan/fc/dam/mobile/telaInicial.java
@@ -1,6 +1,8 @@
 package ao.uan.fc.dam.mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
