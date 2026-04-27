d0a9b7d7dfeec6d5bb411daa2145655b24bdf238
Victor Wanga <victorwanga99@gmail.com>
2026-04-27 13:22:55 +0100

    Estrutura do projecto MVVm e implementacao dos wireframes

--- aplicacao-movel/app/src/main/java/ao/uan/fc/dam/mobile/ui/telaInicial.java
+++ aplicacao-movel/app/src/main/java/ao/uan/fc/dam/mobile/ui/telaInicial.java
@@ -1,4 +1,4 @@
-package ao.uan.fc.dam.mobile;
+package ao.uan.fc.dam.mobile.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,9 +7,8 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import ao.uan.fc.dam.mobile.R;
 
 public class telaInicial extends AppCompatActivity {
 
